9aa2ac809c0f739a901b299dfcd9e82a909ab369
ghazal-git <ghazal@modulbuero.com>
2026-02-02 15:58:05 +0100

    add card, still in progress

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,9 @@
 (($) => {
-	$(document).ready(()=>{
+  $(document).ready(() => {
+    // Click on image area toggles collapse
+    $(document).on('click', '.mmsi-feature .mmsi-feature__media', function () {
+      $(this).closest('.mmsi-feature').toggleClass('is-collapsed');
+    });
+  });
+})(jQuery);
 
-    })
-})(jQuery)
