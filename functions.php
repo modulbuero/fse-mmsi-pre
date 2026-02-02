1c829868485ad76d23b143b729b8e40016771e67
MarkusModulbuero <61046850+MarkusModulbuero@users.noreply.github.com>
2026-02-02 09:20:41 +0100

    Start

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php 
+/**
+ * Load function Files
+ */
+$theme_directory = 'functions/';
+require_once($theme_directory.'enqueue-scripts.php');
+require_once($theme_directory.'options.php');
---
+++ functions/enqueue-scripts.php
@@ -0,0 +1,40 @@
+<?php 
+/**
+ * 	Collecttion of Style-/CSS-files
+ */
+function mbfse_child_style_Files(){
+	$f_ass = '/assets/';
+	$f_CSS = $f_ass.'scss/';
+	$f_JS  = $f_ass.'js/';
+
+	wp_enqueue_style(
+		'mb-child-main',
+		get_stylesheet_directory_uri().'/style.css',
+		array('mbfse-menu-mobile'),
+		wp_get_theme()->get( 'Version' )
+	);
+
+	/*CSS*/
+	wp_enqueue_style(
+		'mb-child-header',
+		get_stylesheet_directory_uri().$f_CSS.'header.css',
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+
+	wp_enqueue_style(
+		'mb-child-content',
+		get_stylesheet_directory_uri().$f_CSS.'content.css',
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+
+	/*JS*/
+	wp_enqueue_script(
+		'mb-child-script',
+		get_stylesheet_directory_uri().$f_JS.'scripts.js',
+		array('jquery', 'mbfse-main'),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action('wp_enqueue_scripts', 'mbfse_child_style_Files', 999);
