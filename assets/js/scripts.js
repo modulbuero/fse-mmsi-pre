ac1b9aa421ec3606754bcac5f8ed46634df455c8
ghazal-git <ghazal@modulbuero.com>
2026-02-10 15:23:11 +0100

    fix responsive mobile size

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,13 @@
 (($) => {
   $(document).ready(() => {
-    // Click on image area toggles collapse
+
     $(document).on('click', '.mmsi-feature .mmsi-feature__media', function () {
+      if (window.innerWidth < 600) return;
+
       $(this).closest('.mmsi-feature').toggleClass('is-collapsed');
     });
+
   });
 })(jQuery);
+
 
