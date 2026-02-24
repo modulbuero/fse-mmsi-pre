9c811ce293a0d6498ee864386d9963237b5088f3
ghazal-git <ghazal@modulbuero.com>
2026-02-24 14:16:19 +0100

    Fix contact form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,25 @@
 (($) => {
   $(document).ready(() => {
 
+    // feature toggle
     $(document).on('click', '.mmsi-feature .mmsi-feature__media', function () {
       if (window.innerWidth < 600) return;
-
       $(this).closest('.mmsi-feature').toggleClass('is-collapsed');
+    });
+
+    // floating label
+    function updateFieldState(input) {
+      const $field = $(input).closest('.nf-field');
+      if ($(input).val().trim() !== '') {
+        $field.addClass('has-value');
+      } else {
+        $field.removeClass('has-value');
+      }
+    }
+    // On typing
+    $(document).on('input', '.nf-field-element input, .nf-field-element textarea', function () {
+      updateFieldState(this);
     });
 
   });
 })(jQuery);
-
-
